6f89df6709d4c6ce62c55682b944f11f9371c3e1
Joserafpg <payerogarciaj@gmail.com>
2023-11-01 14:15:24 -0400

    Version 4.7.1

---
+++ Proyecto Computer/Clases/Clases Inventario/DatosgetInventario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Computer.Clases.Clases_Inventario
+{
+    internal class DatosgetInventario
+    {
+    }
+}
